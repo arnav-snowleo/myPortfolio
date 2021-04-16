18aaf780a5f2546db8a4aad96106fe3de25ee8f8
arnav-snowleo <prakashsinghaarav@gmail.com>
2021-04-16 12:05:08 +0530

    projects.

--- js/all.js
+++ js/all.js
@@ -2,3 +2,14 @@ AOS.init({
     duration: 3000,
   })
 
+//typing animation
+
+var options = {
+  strings: ["Projects ^1000","Apps ^1000" , "Websites ^1000"],
+  typeSpeed: 100,
+  backSpeed: 60,
+ 
+  loop: true,
+};
+
+var typed = new Typed(".metyping",options);
